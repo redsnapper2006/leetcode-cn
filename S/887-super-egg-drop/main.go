ce8dad7a050a0cc07106da6f709aa7c681e42748
Yan Du <red.snapper@qq.com>
2020-04-11 14:19:00 +0800

    enhance 887

--- S/887-super-egg-drop/main.go
+++ S/887-super-egg-drop/main.go
@@ -4,55 +4,80 @@ import (
 	"fmt"
 )
 
-var (
-	matrix [][]int
-)
-
 func superEggDrop(K int, N int) int {
-	matrix = make([][]int, K+1)
-	for i := 0; i < K+1; i++ {
-		matrix[i] = make([]int, N+1)
-		for j, _ := range matrix[i] {
-			matrix[i][j] = -1
-		}
+	// F(K,N) = min(1..n, max(F(K-1,M), F(K,N-M)))
+	if K == 1 {
+		return N
 	}
-	return superEggDropRecur(K, N)
-}
-
-func superEggDropRecur(K int, N int) int {
-	for i := 1; i <= N; i++ {
-		matrix[1][i] = i
+	if N <= 2 {
+		return N
+	}
+	buf := make([][]int, N+1)
+	for i := 0; i < len(buf); i++ {
+		buf[i] = make([]int, K+1)
 	}
 
 	for i := 1; i <= K; i++ {
-		matrix[i][1] = 1
-		matrix[i][2] = 2
+		buf[1][i] = 1
+	}
+	for i := 1; i <= N; i++ {
+		buf[i][1] = i
 	}
 
-	for i := 2; i <= K; i++ {
-		for j := 3; j <= N; j++ {
-			times := N + 1
-			for m := 2; m <= j; m++ {
-				left := matrix[i-1][m-1]
-				right := matrix[i][j-m]
-				times_t := -1
-				if left > right {
-					times_t = left + 1
-				} else {
-					times_t = right + 1
-				}
-				if times_t < times {
-					times = times_t
-				}
+	for j := 2; j <= N; j++ {
+		for i := 2; i <= K; i++ {
+			s := buf[j-1][i-1] + buf[j-1][i] + 1
+			buf[j][i] = s
+			if s >= N {
+				return j
 			}
-			matrix[i][j] = times
 		}
 	}
-	return matrix[K][N]
+	return 0
+}
+
+func superEggDropV2(K int, N int) int {
+	// F(K,N) = min(1..n, max(F(K-1,M), F(K,N-M)))
+
+	buf := make([][]int, N+1)
+	for i := 0; i < len(buf); i++ {
+		buf[i] = make([]int, K+1)
+	}
+
+	for i := 1; i <= K; i++ {
+		buf[1][i] = 1
+	}
+	for i := 1; i <= N; i++ {
+		buf[i][1] = i
+	}
+	if K == 1 {
+		return buf[N][1]
+	}
+	if N == 1 {
+		return buf[1][K]
+	}
+
+	for j := 2; j <= N; j++ {
+		for i := 2; i <= K; i++ {
+			c := N + 1
+			for n := 1; n <= j; n++ {
+				t := buf[n-1][i-1]
+				if buf[n-1][i-1] < buf[j-n][i] {
+					t = buf[j-n][i]
+				}
+				if c > t+1 {
+					c = t + 1
+				}
+			}
+			buf[j][i] = c
+		}
+	}
+	return buf[N][K]
 }
 
 func main() {
 	fmt.Println(superEggDrop(2, 2))
-
+	fmt.Println(superEggDrop(2, 6))
+	fmt.Println(superEggDrop(3, 14))
 	fmt.Println(superEggDrop(100, 10000))
 }
