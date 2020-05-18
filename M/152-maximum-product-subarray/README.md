47e41d5aef1a90ff5de3f6cafb71f510f2f2c2e6
Yan Du <red.snapper@qq.com>
2020-05-18 17:42:50 +0800

    No152 tuning up by using DP

--- M/152-maximum-product-subarray/README.md
+++ M/152-maximum-product-subarray/README.md
@@ -3,3 +3,5 @@
 https://leetcode-cn.com/problems/maximum-product-subarray/
 
 **NEED PERFORMANCE ENHANCEMENT**
+
+# Using DP To Tuning
--- M/152-maximum-product-subarray/main.go
+++ M/152-maximum-product-subarray/main.go
@@ -5,6 +5,51 @@ import (
 )
 
 func maxProduct(nums []int) int {
+	buf := make([][]int, 2)
+	for i := 0; i < 2; i++ {
+		buf[i] = make([]int, len(nums))
+	}
+	buf[0][0] = nums[0]
+	buf[1][0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] >= 0 {
+			t := buf[0][i-1] * nums[i]
+			if t < nums[i] {
+				t = nums[i]
+			}
+			buf[0][i] = t
+			t = buf[1][i-1] * nums[i]
+			if t > nums[i] {
+				t = nums[i]
+			}
+			buf[1][i] = t
+		} else {
+			t := buf[1][i-1] * nums[i]
+			if t < nums[i] {
+				t = nums[i]
+			}
+			buf[0][i] = t
+			t = buf[0][i-1] * nums[i]
+			if t > nums[i] {
+				t = nums[i]
+			}
+			buf[1][i] = t
+		}
+	}
+	max := -1 << 31
+	for i := 0; i < len(buf[0]); i++ {
+		if max < buf[0][i] {
+			max = buf[0][i]
+		}
+		if max < buf[1][i] {
+			max = buf[1][i]
+		}
+	}
+
+	return max
+}
+
+func maxProductV2(nums []int) int {
 	max := nums[0]
 	for i := 0; i < len(nums); i++ {
 		c := nums[i]
@@ -23,5 +68,5 @@ func maxProduct(nums []int) int {
 
 func main() {
 	fmt.Println("a")
-	fmt.Println(maxProduct([]int{-2, 0, -1}))
+	fmt.Println(maxProduct([]int{2, -5, -2, -4, 3}))
 }
