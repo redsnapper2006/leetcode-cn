2a19fcf98d0dbf235cf60f1f2886803289fe7e46
Yan Du <red.snapper@qq.com>
2020-07-02 10:53:28 +0800

    fix

--- M/53-maximum-subarray/main.go
+++ M/53-maximum-subarray/main.go
@@ -9,15 +9,15 @@ func maxSubArray(nums []int) int {
 	if len(nums) == 1 {
 		return nums[0]
 	}
-	buf :=make ([]int, len(nums))
+	buf := make([]int, len(nums))
 
 	buf[0] = nums[0]
-	max = buf[0]
-	for i:=1;i<len(nums);i++ {
-		if buf[i-1] + nums[i] > nums[i] {
+	max := buf[0]
+	for i := 1; i < len(nums); i++ {
+		if buf[i-1]+nums[i] > nums[i] {
 			buf[i] = buf[i-1] + nums[i]
 		} else {
-			buf[i] =  nums[i]
+			buf[i] = nums[i]
 		}
 		if buf[i] > max {
 			max = buf[i]
@@ -30,5 +30,5 @@ func main() {
 	// fmt.Println(maxSubArray([]int{-2, -1}))
 	// fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
 	// fmt.Println(maxSubArray([]int{2, 1}))
-	fmt.Println(maxSubArray([]int{-2, -1,-2}))
+	fmt.Println(maxSubArray([]int{-2, -1, -2}))
 }
