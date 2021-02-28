a5f68bce219066356fac7b86afc4ee063791483a
Yan Du <red.snapper@qq.com>
2021-02-28 16:29:19 +0800

    add 1749-maximum-absolute-sum-of-any-subarray

---
+++ M/1749-maximum-absolute-sum-of-any-subarray/README.md
@@ -0,0 +1,3 @@
+# No1749
+
+https://leetcode-cn.com/problems/maximum-absolute-sum-of-any-subarray/
---
+++ M/1749-maximum-absolute-sum-of-any-subarray/main.rs
@@ -0,0 +1,29 @@
+struct Solution {}
+
+impl Solution {
+  pub fn max_absolute_sum(nums: Vec<i32>) -> i32 {
+    let mut sum = 0;
+    let mut buf: Vec<i32> = vec![0];
+    let mut ret = 0;
+    let mut min = 0;
+    let mut max = 0;
+    for i in 0..nums.len() {
+      sum += nums[i];
+      if sum > max {
+        max = sum;
+      }
+      if sum < min {
+        min = sum;
+      }
+      let diff = max - min;
+      if diff > ret {
+        ret = diff
+      }
+    }
+    ret
+  }
+}
+
+fn main() {
+  println!("{}", Solution::max_absolute_sum(vec![0; 5]));
+}
