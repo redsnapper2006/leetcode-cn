70004535e9d21c78f7083a38a402036336e88a29
Yan Du <red.snapper@qq.com>
2021-06-27 11:23:52 +0800

    add 5797-maximum-product-difference-between-two-pairs

---
+++ M/5797-maximum-product-difference-between-two-pairs/README.md
@@ -0,0 +1,3 @@
+# No5797
+
+https://leetcode-cn.com/contest/weekly-contest-247/problems/maximum-product-difference-between-two-pairs/
---
+++ M/5797-maximum-product-difference-between-two-pairs/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func maxProductDifference(nums []int) int {
+	sort.Ints(nums)
+	return nums[len(nums)-1]*nums[len(nums)-2] - nums[0]*nums[1]
+}
+
+func main() {
+	fmt.Println()
+}
