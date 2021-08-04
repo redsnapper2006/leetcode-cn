47238a9545c699ea05b24900e9270eeef5f968c0
Yan Du <red.snapper@qq.com>
2021-08-04 10:57:11 +0800

    add new impl

--- V/611-valid-triangle-number/main.rs
+++ V/611-valid-triangle-number/main.rs
@@ -1,19 +1,17 @@
 struct Solution {}
 
 impl Solution {
-  pub fn triangle_number(mut nums: Vec<i32>) -> i32 {
+  pub fn triangle_number2(mut nums: Vec<i32>) -> i32 {
     if nums.len() < 3 {
       return 0;
     }
     nums.sort();
-    // println!("{:?}", nums);
     let mut sum: i32 = 0;
     for i in 0..nums.len() - 2 {
       for j in (i + 1)..nums.len() - 1 {
         let ll = nums[i] + nums[j];
         let mut s: i32 = j as i32 + 1;
         let mut e: i32 = nums.len() as i32 - 1;
-        // println!("{} {} {} {}", nums[i], nums[j], s, e);
         while s <= e {
           let m = s + (e - s) / 2;
           if nums[m as usize] >= ll {
@@ -23,7 +21,25 @@ impl Solution {
           }
         }
         sum += s - j as i32 - 1;
-        // println!("{} {} ", s, sum);
+      }
+    }
+    sum
+  }
+
+  pub fn triangle_number(mut nums: Vec<i32>) -> i32 {
+    if nums.len() < 3 {
+      return 0;
+    }
+    nums.sort();
+
+    let mut sum: i32 = 0;
+    for i in 0..nums.len() - 2 {
+      for j in (i + 1)..nums.len() - 1 {
+        let mut k: usize = j + 1;
+        while k < nums.len() && nums[i] + nums[j] > nums[k] {
+          k += 1;
+        }
+        sum += (k - j) as i32 - 1;
       }
     }
     sum
