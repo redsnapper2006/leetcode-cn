d141db01707961949d237b8d364a5eeec4d52510
Yan Du <red.snapper@qq.com>
2022-11-12 22:58:01 +0800

    refactor

--- M/1749-maximum-absolute-sum-of-any-subarray/main.rs
+++ M/1749-maximum-absolute-sum-of-any-subarray/main.rs
@@ -3,7 +3,7 @@ struct Solution {}
 impl Solution {
   pub fn max_absolute_sum(nums: Vec<i32>) -> i32 {
     let mut sum = 0;
-    let mut buf: Vec<i32> = vec![0];
+    // let mut buf: Vec<i32> = vec![0];
     let mut ret = 0;
     let mut min = 0;
     let mut max = 0;
