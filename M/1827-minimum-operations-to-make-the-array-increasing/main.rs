fe77f07bd3a7f298e77a3163eb2ff3504170b08a
Yan Du <red.snapper@qq.com>
2022-12-11 10:25:21 +0800

    update

--- M/1827-minimum-operations-to-make-the-array-increasing/main.rs
+++ M/1827-minimum-operations-to-make-the-array-increasing/main.rs
@@ -1,17 +1,16 @@
 struct Solution {}
 
-
 impl Solution {
-    pub fn min_operations(nums: Vec<i32>) -> i32 {
-      let mut copy = nums.clone();
-      let mut ret :i32 = 0;
-      for i in 1..copy.len() {
-        if copy[i] > copy[i-1] {
-          continue;
-        }
-        ret +=  copy[i-1]+1-copy[i];
-        copy[i] = copy[i-1]+1;
+  pub fn min_operations(nums: Vec<i32>) -> i32 {
+    let mut copy = nums.clone();
+    let mut ret: i32 = 0;
+    for i in 1..copy.len() {
+      if copy[i] > copy[i - 1] {
+        continue;
       }
-      ret
+      ret += copy[i - 1] + 1 - copy[i];
+      copy[i] = copy[i - 1] + 1;
     }
+    ret
+  }
 }
