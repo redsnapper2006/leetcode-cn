ab9b0261463b8a3cfce2d79b242383184270a495
Yan Du <red.snapper@qq.com>
2023-10-30 12:21:48 +0800

    add H/275-h-index-ii/main.rs

---
+++ H/275-h-index-ii/main.rs
@@ -0,0 +1,17 @@
+struct Solution {}
+
+struct Solution {}
+
+impl Solution {
+  pub fn h_index(citations: Vec<i32>) -> i32 {
+    let mut citations = citations;
+    citations.sort();
+    let mut h = 0;
+    (0..citations.len()).rev().for_each(|i| {
+      if citations[i] > h as i32 {
+        h += 1;
+      }
+    });
+    h
+  }
+}
