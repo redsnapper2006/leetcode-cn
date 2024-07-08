5b46924a479c2873d18c456ee1df8fa85648deca
Yan Du <red.snapper@qq.com>
2024-07-08 12:02:30 +0800

    ✨ Feat: add F/724-find-pivot-index/main.rs

--- F/724-find-pivot-index/main.rs
+++ F/724-find-pivot-index/main.rs
@@ -1,32 +1,31 @@
-use std::convert::TryInto;
-
 struct Solution {}
 
 impl Solution {
-    pub fn pivot_index(nums: Vec<i32>) -> i32 {
-        let mut sums = vec![0; nums.len()];
-        let mut sum = 0;
-        for i in 0..nums.len() {
-            sum += nums[i];
-            sums[i] = sum;
-        }
-        let mut ret: i32 = -1;
-        for i in 0..nums.len() {
-            let mut p = 0;
-            let n = sum - sums[i];
-            if i > 0 {
-                p = sums[i] - nums[i];
-            }
-            if p == n {
-                ret = i.try_into().unwrap();
-                break;
-            }
-        }
-        ret
+  pub fn pivot_index(nums: Vec<i32>) -> i32 {
+    let mut left: Vec<i32> = vec![0; nums.len()];
+    let mut right: Vec<i32> = vec![0; nums.len()];
+    let mut ls: i32 = 0;
+    let mut rs: i32 = 0;
+
+    (0..nums.len()).for_each(|idx| {
+      left[idx] = ls;
+      right[nums.len() - 1 - idx] = rs;
+      ls += nums[idx];
+      rs += nums[nums.len() - 1 - idx];
+    });
+
+    let mut idx: usize = 0;
+    while idx < nums.len() {
+      if left[idx] == right[idx] {
+        return idx as i32;
+      }
+
+      idx += 1;
     }
+    -1
+  }
 }
 
 fn main() {
-    println!("helloworld");
-    println!("{}", Solution::pivot_index(vec![1, 7, 3, 6, 5, 6]));
+  println!("{}", Solution::pivot_index(vec![1, 7, 3, 6, 5, 6]));
 }
