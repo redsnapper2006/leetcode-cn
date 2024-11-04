08dd17ada0e4665666d7f37861f6fe2acd6a8e47
Yan Du <red.snapper@qq.com>
2024-11-04 08:33:55 +0800

    style: 🎨  add S/633-sum-of-square-numbers/main.rs

--- S/633-sum-of-square-numbers/main.rs
+++ S/633-sum-of-square-numbers/main.rs
@@ -2,8 +2,9 @@ struct Solution {}
 
 impl Solution {
   pub fn judge_square_sum(c: i32) -> bool {
-    let mut a: i32 = 0;
-    let mut b: i32 = (c as f64).sqrt() as i32;
+    let c = c as i64;
+    let mut a: i64 = 0;
+    let mut b: i64 = (c as f64).sqrt() as i64;
     while a <= b {
       let d = a * a + b * b;
       if d == c {
