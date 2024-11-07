d12b0e146180ad076a561e4ad0149318fbae433d
Yan Du <red.snapper@qq.com>
2024-11-07 09:48:28 +0800

    feature: ✨  add F/3255-find-the-power-of-k-size-subarrays-ii/main.rs

--- F/3255-find-the-power-of-k-size-subarrays-ii/main.rs
+++ F/3255-find-the-power-of-k-size-subarrays-ii/main.rs
@@ -5,21 +5,18 @@ impl Solution {
     nums
       .iter()
       .enumerate()
-      .fold((0, Vec::<i32>::new()), |(cnt, mut ans), (idx, v)| {
-        if idx + 1 >= k as usize {
-          if k == 1 {
-            ans.push(nums[idx]);
-          } else if nums[idx] == nums[idx - 1] + 1 {
-            ans.push(if cnt + 1 >= k { nums[idx] } else { -1 });
-          } else {
-            ans.push(-1);
-          }
-        }
-        if k > 1 && idx > 0 && nums[idx] == nums[idx - 1] + 1 {
-          (cnt + 1, ans)
+      .fold((0, Vec::<i32>::new()), |(mut cnt, mut ans), (idx, v)| {
+        if idx == 0 || nums[idx] != nums[idx - 1] + 1 {
+          cnt = 1;
         } else {
-          (1, ans)
+          cnt += 1;
         }
+        if cnt >= k as usize {
+          ans.push(nums[idx]);
+        } else if idx + 1 >= k as usize {
+          ans.push(-1);
+        }
+        (cnt, ans)
       })
       .1
   }
