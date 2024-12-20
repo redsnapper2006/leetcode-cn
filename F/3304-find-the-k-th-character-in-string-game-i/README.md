33eab32cf3922c460490c1f7d32807a53ae978d6
Yan Du <red.snapper@qq.com>
2024-12-20 12:16:11 +0800

    feature: ✨  add F/3304-find-the-k-th-character-in-string-game-i/

---
+++ F/3304-find-the-k-th-character-in-string-game-i/README.md
@@ -0,0 +1,3 @@
+# No3304
+
+<https://leetcode.cn/problems/find-the-k-th-character-in-string-game-i/description/>
---
+++ F/3304-find-the-k-th-character-in-string-game-i/main.rs
@@ -0,0 +1,5 @@
+impl Solution {
+  pub fn kth_character(k: i32) -> char {
+    (b'a' + (k - 1).count_ones() as u8) as char
+  }
+}
