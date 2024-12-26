303c904f0b91b36dc9944ac3a1f5907d30df562b
Yan Du <red.snapper@qq.com>
2024-12-26 09:50:53 +0800

    feature: ✨  add E/3083/main.rs

--- E/3083-existence-of-a-substring-in-a-string-and-its-reverse/main.rs
+++ E/3083-existence-of-a-substring-in-a-string-and-its-reverse/main.rs
@@ -17,4 +17,22 @@ impl Solution {
 
     ans
   }
+
+  pub fn is_substring_present(s: String) -> bool {
+    let mut dp: Vec<Vec<bool>> = vec![vec![false; 101]; 101];
+
+    let bb = s.as_bytes().to_vec();
+    let mut idx: usize = 1;
+    while idx < bb.len() {
+      let off1 = (bb[idx] - b'a') as usize;
+      let off2 = (bb[idx - 1] - b'a') as usize;
+      dp[off1][off2] = true;
+      if dp[off2][off1] {
+        return true;
+      }
+      idx += 1;
+    }
+
+    false
+  }
 }
