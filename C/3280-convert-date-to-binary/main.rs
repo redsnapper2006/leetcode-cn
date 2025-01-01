21889aedbda54932d84600f1dbe3c81af04ca505
Yan Du <red.snapper@qq.com>
2025-01-01 13:12:57 +0800

    feature: ✨  add C/3280-convert-date-to-binary/main.rs

---
+++ C/3280-convert-date-to-binary/main.rs
@@ -0,0 +1,11 @@
+struct Solution {}
+
+impl Solution {
+  pub fn convert_date_to_binary(date: String) -> String {
+    date
+      .split('-')
+      .map(|s| format!("{:b}", s.parse::<u16>().unwrap()))
+      .collect::<Vec<_>>()
+      .join("-")
+  }
+}
