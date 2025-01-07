e4819caafda05b12c1214d1e2b3545f648aac374
Yan Du <red.snapper@qq.com>
2025-01-07 09:15:51 +0800

    feature: ✨  add N/3019-number-of-changing-keys/main.rs

--- N/3019-number-of-changing-keys/main.rs
+++ N/3019-number-of-changing-keys/main.rs
@@ -1,5 +1,18 @@
 impl Solution {
   pub fn count_key_changes(s: String) -> i32 {
+    let bb = s.as_bytes().to_vec();
+    let mut cnt: i32 = 0;
+    (1..bb.len()).for_each(|idx| {
+      cnt += if bb[idx] & 31 != bb[idx - 1] & 31 {
+        1
+      } else {
+        0
+      };
+    });
+    cnt
+  }
+
+  pub fn count_key_changes2(s: String) -> i32 {
     let bb = s.to_lowercase().as_bytes().to_vec();
 
     let mut base = bb[0] - b'a';
