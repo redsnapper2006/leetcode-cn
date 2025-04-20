24bfeca6c7d8bac49ed5b91442283be650a28ee6
Yan Du <red.snapper@qq.com>
2025-04-20 09:30:57 +0800

    feature: ✨  update R/781-rabbits-in-forest/main.rs

--- R/781-rabbits-in-forest/main.rs
+++ R/781-rabbits-in-forest/main.rs
@@ -4,6 +4,22 @@ use std::collections::HashMap;
 impl Solution {
   pub fn num_rabbits(mut answers: Vec<i32>) -> i32 {
     let mut m: HashMap<i32, i32> = HashMap::new();
+    answers.iter().for_each(|&v| {
+      m.entry(v).and_modify(|x| *x += 1).or_insert(1);
+    });
+    let mut ans: i32 = 0;
+    for (k, v) in m {
+      let mut v = v;
+      while v > 0 {
+        v -= k + 1;
+        ans += k + 1;
+      }
+    }
+    ans
+  }
+
+  pub fn num_rabbits2(mut answers: Vec<i32>) -> i32 {
+    let mut m: HashMap<i32, i32> = HashMap::new();
     answers.sort();
 
     let mut p = -1;
