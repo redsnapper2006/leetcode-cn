f44f058840839dd34099c0ad7f53bf8d7ebac251
Yan Du <red.snapper@qq.com>
2025-06-08 10:02:12 +0800

    feature: ✨  update

--- L/386-lexicographical-numbers/main.rs
+++ L/386-lexicographical-numbers/main.rs
@@ -1,5 +1,5 @@
 impl Solution {
-  pub fn lexical_order(n: i32) -> Vec<i32> {
+  pub fn lexical_order2(n: i32) -> Vec<i32> {
     fn dfs(b: i32, n: i32, ans: &mut Vec<i32>) {
       if b > n {
         return;
@@ -13,6 +13,26 @@ impl Solution {
     (1..10).for_each(|idx| {
       dfs(idx, n, &mut ans);
     });
+    ans
+  }
+
+  pub fn lexical_order(n: i32) -> Vec<i32> {
+    let mut b: i32 = 1;
+    let mut ans: Vec<i32> = vec![];
+
+    for _ in 0..n {
+      ans.push(b);
+      if b * 10 <= n {
+        b *= 10;
+        continue;
+      } else {
+        while b % 10 == 9 || b + 1 > n {
+          b /= 10;
+        }
+        b += 1;
+      }
+    }
+
     ans
   }
 }
