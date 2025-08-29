8268f1ca252a89188dacb7fdf0011bb40906874f
Yan Du <red.snapper@qq.com>
2025-08-29 10:42:19 +0800

    feature: ✨  A/3021-alice-and-bob-playing-flower-game/

---
+++ A/3021-alice-and-bob-playing-flower-game/README..md
@@ -0,0 +1,3 @@
+# No3021
+
+<https://leetcode.cn/problems/alice-and-bob-playing-flower-game>
---
+++ A/3021-alice-and-bob-playing-flower-game/main.rs
@@ -0,0 +1,7 @@
+impl Solution {
+  pub fn flower_game(n: i32, m: i32) -> i64 {
+    let n = n as i64;
+    let m = m as i64;
+    (n * m ) / 2
+  }
+}
