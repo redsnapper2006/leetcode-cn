e219ac3f0bfeb78269af75a418b653cde039ef85
Yan Du <red.snapper@qq.com>
2025-09-03 20:21:40 +0800

    feature: ✨  F/3027-find-the-number-of-ways-to-place-people-ii/

---
+++ F/3027-find-the-number-of-ways-to-place-people-ii/README.md
@@ -0,0 +1,3 @@
+# No3027
+
+<https://leetcode.cn/problems/find-the-number-of-ways-to-place-people-ii>
---
+++ F/3027-find-the-number-of-ways-to-place-people-ii/main.rs
@@ -0,0 +1,26 @@
+impl Solution {
+  pub fn number_of_pairs(points: Vec<Vec<i32>>) -> i32 {
+    let mut points = points;
+    points.sort_by(|p1, p2| {
+      if p1[0] != p2[0] {
+        return p2[0].cmp(&p1[0]);
+      }
+      p1[1].cmp(&p2[1])
+    });
+
+    let mut ans: i32 = 0;
+    for i in 0..points.len() {
+      let mut ref_y: i32 = i32::MIN;
+      for j in i + 1..points.len() {
+        if points[j][1] < points[i][1] {
+          continue;
+        }
+        if ref_y == i32::MIN || points[j][1] < ref_y {
+          ref_y = points[j][1];
+          ans += 1;
+        }
+      }
+    }
+    ans
+  }
+}
