6db4b23da2bcc5a51f3ce8e046ecb23a2bb271d4
Yan Du <red.snapper@qq.com>
2025-09-11 10:37:44 +0800

    feature: ✨  S/2785-sort-vowels-in-a-string/main.rs

--- S/2785-sort-vowels-in-a-string/main.rs
+++ S/2785-sort-vowels-in-a-string/main.rs
@@ -1,6 +1,31 @@
 impl Solution {
   pub fn sort_vowels(s: String) -> String {
     let mut res: Vec<u8> = Vec::new();
+    let mut cnt: Vec<i32> = vec![0; (b'z' - b'A') as usize + 1];
+    s.as_bytes().iter().for_each(|&v| {
+      if "AEIOUaeiou".contains(v as char) {
+        cnt[(v - b'A') as usize] += 1;
+      }
+    });
+
+    let mut off: usize = 0;
+    s.as_bytes().iter().for_each(|&v| {
+      if "AEIOUaeiou".contains(v as char) {
+        while cnt[off] == 0 {
+          off += 1;
+        }
+        cnt[off] -= 1;
+        res.push((b'A' + off as u8));
+      } else {
+        res.push(v);
+      }
+    });
+
+    String::from_utf8(res).unwrap()
+  }
+
+  pub fn sort_vowels2(s: String) -> String {
+    let mut res: Vec<u8> = Vec::new();
     let mut idxs: Vec<usize> = Vec::new();
     let mut vowels: Vec<u8> = Vec::new();
     s.as_bytes().iter().enumerate().for_each(|(idx, &v)| {
