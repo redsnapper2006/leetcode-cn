a5a74c332fcb5410dee19106b1d348e0fcda2e05
Yan Du <red.snapper@qq.com>
2025-09-12 11:02:18 +0800

    feature: ✨  S/2785-sort-vowels-in-a-string/main.rs

--- V/3227-vowels-game-in-a-string/main.rs
+++ V/3227-vowels-game-in-a-string/main.rs
@@ -1,5 +1,18 @@
 impl Solution {
   pub fn does_alice_win(s: String) -> bool {
+    for c in s.chars() {
+      if "aeiou".contains(c) {
+        return true;
+      }
+    }
+    false
+  }
+
+  pub fn does_alice_win2(s: String) -> bool {
+    s.chars().any(|x| "aeiou".contains(x))
+  }
+
+  pub fn does_alice_win3(s: String) -> bool {
     s.as_bytes()
       .to_vec()
       .iter()
