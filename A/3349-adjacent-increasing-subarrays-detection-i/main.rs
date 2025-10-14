c045e89483a9cb7925e4d04e9144f3e3020d30d8
Yan Du <red.snapper@qq.com>
2025-10-14 10:01:12 +0800

    feat: ✨ A/3349-adjacent-increasing-subarrays-detection-i/main.rs

--- A/3349-adjacent-increasing-subarrays-detection-i/main.rs
+++ A/3349-adjacent-increasing-subarrays-detection-i/main.rs
@@ -1,5 +1,37 @@
 impl Solution {
   pub fn has_increasing_subarrays(nums: Vec<i32>, k: i32) -> bool {
+    let mut cnt : i32 = 1;
+    let mut times : i32 = 0;
+    for i in 1..nums.len() {
+      if nums[i] > nums[i-1] {
+        cnt += 1;
+      } else {
+        if cnt >= 2 * k {
+          return true;
+        }
+        if cnt >= k {
+          if times == 1 {
+            return true;
+          }
+          times = 1;
+        } else {
+          times = 0;
+        }
+        cnt = 1;
+      }
+    }
+    if cnt >= 2 * k {
+      return true;
+    }
+    if cnt >= k && times == 1 {
+      true
+    } else {
+      false
+    }
+  }
+
+
+  pub fn has_increasing_subarrays2(nums: Vec<i32>, k: i32) -> bool {
     let mut left: Vec<i32> = vec![-1; nums.len()];
     let mut right: Vec<i32> = vec![-1; nums.len()];
     (0..nums.len()).for_each(|idx| {
