f52c0053bc3bdba008f060e673535b745ffd9cfd
Yan Du <red.snapper@qq.com>
2025-10-18 12:04:13 +0800

    refactor: ♻️ M/3397-maximum-number-of-distinct-elements-after-operations/main.rs

--- M/3397-maximum-number-of-distinct-elements-after-operations/main.rs
+++ M/3397-maximum-number-of-distinct-elements-after-operations/main.rs
@@ -2,12 +2,10 @@ impl Solution {
   pub fn max_distinct_elements(nums: Vec<i32>, k: i32) -> i32 {
     let mut nums = nums;
     nums.sort_unstable();
-    let mn = nums.iter().min().unwrap() - k;
-    let mx = nums.iter().max().unwrap() + k;
     let mut idx: usize = 0;
     let mut ans: i32 = 0;
-    let mut i: i32 = mn;
-    while i <= mx {
+    let mut i: i32 = nums[0] - k;
+    while i <= nums[nums.len() - 1] + k {
       while nums[idx] + k < i {
         idx += 1;
       }
