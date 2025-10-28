51de62983906c6a0b203d780fc77b30a8e631fb0
Yan Du <red.snapper@qq.com>
2025-10-28 10:33:38 +0800

    feat: ✨ M/3354-make-array-elements-equal-to-zero/main.rs

--- M/3354-make-array-elements-equal-to-zero/main.rs
+++ M/3354-make-array-elements-equal-to-zero/main.rs
@@ -1,20 +1,19 @@
 impl Solution {
   pub fn count_valid_selections(nums: Vec<i32>) -> i32 {
     let total = nums.iter().sum::<i32>();
-
-    let mut sum: i32 = 0;
-    let mut ans: i32 = 0;
-    (0..nums.len()).for_each(|idx| {
-      sum += nums[idx];
-      if nums[idx] == 0 {
-        if sum * 2 == total {
-          ans += 2;
-        } else if sum * 2 + 1 == total || sum * 2 - 1 == total {
-          ans += 1;
-        }
-      }
-    });
-
-    ans
+    nums
+      .iter()
+      .fold((0, 0), |(ans, sum), &v| {
+        (
+          ans
+            + if v == 0 {
+              (2 - ((sum + v) * 2 - total).abs()).max(0)
+            } else {
+              0
+            },
+          sum + v,
+        )
+      })
+      .0
   }
 }
