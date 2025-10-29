7bd9f7b26f839fea65ac0965507eeef90ba6709a
Yan Du <red.snapper@qq.com>
2025-10-29 10:27:07 +0800

    feat: ✨ S/3370-smallest-number-with-all-set-bits/main.rs

--- S/3370-smallest-number-with-all-set-bits/main.rs
+++ S/3370-smallest-number-with-all-set-bits/main.rs
@@ -1,12 +1,9 @@
 impl Solution {
   pub fn smallest_number(n: i32) -> i32 {
-    let mut m = n;
-    let mut offset: i32 = 0;
-    while m > 0 {
-      m /= 2;
-      offset += 1;
+    let mut ans: i32 = 1;
+    while ans <= n {
+      ans <<= 1;
     }
-
-    ((1 << offset) - 1) | n
+    ans - 1
   }
 }
