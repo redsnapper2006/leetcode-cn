842894d71738f4df1161a0bd329cc1dc74331187
Yan Du <red.snapper@qq.com>
2025-10-31 10:05:04 +0800

    feat: ✨ T/3289-the-two-sneaky-numbers-of-digitville/main.rs

--- T/3289-the-two-sneaky-numbers-of-digitville/main.rs
+++ T/3289-the-two-sneaky-numbers-of-digitville/main.rs
@@ -1,5 +1,19 @@
 impl Solution {
   pub fn get_sneaky_numbers(nums: Vec<i32>) -> Vec<i32> {
+    let mut buf: i128 = 0;
+    nums.iter().for_each(|&v| {
+      buf ^= (1 << v);
+    });
+    let mut ans: Vec<i32> = vec![];
+    for i in 0..nums.len() - 2 {
+      if buf & (1 << i) == 0 {
+        ans.push(i as i32);
+      }
+    }
+    ans
+  }
+
+  pub fn get_sneaky_numbers2(nums: Vec<i32>) -> Vec<i32> {
     let mut dp: Vec<i32> = vec![0; nums.len() - 2];
     nums.iter().for_each(|&v| {
       dp[v as usize] += 1;
