100d553ddc7359ced80fa5d87d07e8a14c8a6a65
Yan Du <red.snapper@qq.com>
2025-11-11 10:06:52 +0800

    feat: ✨ O/474-ones-and-zeroes/main.rs

--- O/474-ones-and-zeroes/main.rs
+++ O/474-ones-and-zeroes/main.rs
@@ -2,37 +2,27 @@ struct Solution {}
 
 impl Solution {
   pub fn find_max_form(strs: Vec<String>, m: i32, n: i32) -> i32 {
-    let mut buf: Vec<Vec<i32>> = vec![vec![0; n as usize + 1]; m as usize + 1];
+    let m = m as usize;
+    let n = n as usize;
+    let mut buf: Vec<Vec<i32>> = vec![vec![0; n + 1]; m + 1];
     buf[0][0] = 1;
 
     for ss in strs {
-      let mut ones: i32 = 0;
-      let mut zeros: i32 = 0;
+      let mut ones: usize = 0;
+      let mut zeros: usize = 0;
       for b in ss.as_bytes() {
-        if *b == '1' as u8 {
-          ones += 1;
-        } else {
-          zeros += 1;
-        }
+        ones += if *b == '1' as u8 { 1 } else { 0 };
+        zeros += if *b == '1' as u8 { 0 } else { 1 };
       }
       (0..=m).rev().for_each(|mi| {
         (0..=n).rev().for_each(|ni| {
-          if mi - zeros >= 0
-            && ni - ones >= 0
-            && buf[(mi - zeros) as usize][(ni - ones) as usize] > 0
-          {
-            buf[mi as usize][ni as usize] = buf[mi as usize][ni as usize]
-              .max(buf[(mi - zeros) as usize][(ni - ones) as usize] + 1);
+          if mi >= zeros && ni >= ones && buf[mi - zeros][ni - ones] > 0 {
+            buf[mi][ni] = buf[mi][ni].max(buf[mi - zeros][ni - ones] + 1);
           }
         });
       });
     }
 
-    buf
-      .iter()
-      .map(|v| v.iter().fold(-1, |acc, &e| e.max(acc)))
-      .max()
-      .unwrap()
-      - 1
+    buf.iter().map(|v| v.iter().fold(-1, |acc, &e| e.max(acc))).max().unwrap() - 1
   }
 }
