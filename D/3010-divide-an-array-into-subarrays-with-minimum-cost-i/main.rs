f86430fe90b179a49c010b714e5017b37cc71f69
Yan Du <red.snapper@foxmail.com>
2026-02-03 10:36:17 +0800

    feat: ✨ T/3637-trionic-array-i/main.rs

--- D/3010-divide-an-array-into-subarrays-with-minimum-cost-i/main.rs
+++ D/3010-divide-an-array-into-subarrays-with-minimum-cost-i/main.rs
@@ -1,7 +1,17 @@
-struct Solution {}
-
 impl Solution {
   pub fn minimum_cost(nums: Vec<i32>) -> i32 {
+    let (mn1, mn2) = (1..nums.len()).fold((i32::MAX, i32::MAX), |(mn1, mn2), idx| {
+      match (mn1 >= nums[idx], mn2 >= nums[idx]) {
+        (true, true) => (mn2, nums[idx]),
+        (true, false) => (nums[idx], mn2),
+        (_, _) => (mn1, mn2),
+      }
+    });
+
+    nums[0] + mn1 + mn2
+  }
+
+  pub fn minimum_cost2(nums: Vec<i32>) -> i32 {
     let mut nums = nums;
     let mut sum: i32 = nums[0];
     nums.remove(0);
@@ -9,3 +19,5 @@ impl Solution {
     sum + nums[0] + nums[1]
   }
 }
+
+struct Solution {}
--- T/3637-trionic-array-i/main.rs
+++ T/3637-trionic-array-i/main.rs
@@ -2,6 +2,41 @@ use std::cmp::Ordering;
 
 impl Solution {
   pub fn is_trionic(nums: Vec<i32>) -> bool {
+    let mut fidx: usize = 1;
+    while fidx < nums.len() {
+      if nums[fidx] == nums[fidx - 1] {
+        return false;
+      }
+      if nums[fidx] < nums[fidx - 1] {
+        break;
+      }
+      fidx += 1;
+    }
+
+    let mut tidx: usize = nums.len() - 1;
+    while tidx >= 0 {
+      if tidx == 0 || nums[tidx] == nums[tidx - 1] {
+        return false;
+      }
+      if nums[tidx] < nums[tidx - 1] {
+        break;
+      }
+      tidx -= 1;
+    }
+
+    if fidx == 1 || tidx == nums.len() - 1 {
+      return false;
+    }
+    while fidx < tidx {
+      if nums[fidx] <= nums[fidx + 1] {
+        return false;
+      }
+      fidx += 1;
+    }
+    true
+  }
+
+  pub fn is_trionic2(nums: Vec<i32>) -> bool {
     let sort: Vec<std::cmp::Ordering> = vec![Ordering::Greater, Ordering::Less, Ordering::Greater];
     let mut n_idx: usize = 1;
     let mut s_idx: usize = 0;
