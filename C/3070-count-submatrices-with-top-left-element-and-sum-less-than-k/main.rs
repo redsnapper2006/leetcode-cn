a466baeffeb5a77fbd9fe50bb4c9cdb99c33da71
Yan Du <red.snapper@foxmail.com>
2026-03-18 14:04:34 +0800

    feat: ✨ C\3070-count-submatrices-with-top-left-element-and-sum-less-than-k\main.rs

--- C/3070-count-submatrices-with-top-left-element-and-sum-less-than-k/main.rs
+++ C/3070-count-submatrices-with-top-left-element-and-sum-less-than-k/main.rs
@@ -1,5 +1,20 @@
 impl Solution {
   pub fn count_submatrices(grid: Vec<Vec<i32>>, k: i32) -> i32 {
+    let mut buf: Vec<Vec<i32>> = vec![vec![0; grid[0].len() + 1]; 2];
+
+    let mut ans: i32 = 0;
+    (0..grid.len()).for_each(|row| {
+      (1..grid[0].len() + 1).for_each(|col| {
+        buf[row % 2][col] = buf[row % 2][col - 1] + buf[(row + 1) % 2][col]
+          - buf[(row + 1) % 2][col - 1]
+          + grid[row][col - 1];
+        ans += if buf[row % 2][col] <= k { 1 } else { 0 };
+      });
+    });
+    ans
+  }
+
+  pub fn count_submatrices2(grid: Vec<Vec<i32>>, k: i32) -> i32 {
     let mut buf: Vec<Vec<i32>> = vec![vec![0; grid[0].len()]; grid.len()];
 
     (0..grid.len()).for_each(|row| {
