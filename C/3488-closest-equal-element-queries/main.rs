e33846ba8ea3fde65ae8383ed911494ff4a1c527
Yan Du <red.snapper@foxmail.com>
2026-04-16 11:28:32 +0800

    feat: ✨ C/3488-closest-equal-element-queries/main.rs

--- C/3488-closest-equal-element-queries/main.rs
+++ C/3488-closest-equal-element-queries/main.rs
@@ -1,11 +1,51 @@
 use std::collections::HashMap;
 impl Solution {
   pub fn solve_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
+    let n = nums.len();
+    let mut m: HashMap<i32, i32> = HashMap::new();
+
+    let mut left: Vec<i32> = vec![-1; nums.len()];
+    let mut right: Vec<i32> = vec![-1; nums.len()];
+
+    (0..nums.len() * 2).for_each(|idx| {
+      if idx >= n {
+        left[idx % n] = if m.contains_key(&nums[idx % n]) {
+          *m.get(&nums[idx % n]).unwrap()
+        } else {
+          0
+        };
+      }
+      m.insert(nums[idx % n], idx as i32 - n as i32);
+    });
+
+    m.clear();
+    (0..nums.len() * 2).rev().for_each(|idx| {
+      if idx < n {
+        right[idx] = if m.contains_key(&nums[idx]) {
+          *m.get(&nums[idx]).unwrap()
+        } else {
+          0
+        };
+      }
+      m.insert(nums[idx % n], idx as i32);
+    });
+
+    queries
+      .iter()
+      .map(|&q| {
+        if q - left[q as usize] == nums.len() as i32 {
+          -1
+        } else {
+          (q - left[q as usize]).min(right[q as usize] - q)
+        }
+      })
+      .collect()
+  }
+
+  pub fn solve_queries2(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
     let mut m: HashMap<i32, Vec<i32>> = HashMap::new();
     for i in 0..nums.len() {
-      m.entry(nums[i])
-        .and_modify(|x| x.push(i as i32))
-        .or_insert(vec![i as i32]);
+      m.entry(nums[i]).and_modify(|x| x.push(i as i32)).or_insert(vec![i as i32]);
     }
 
     let mut ans: Vec<i32> = vec![];
