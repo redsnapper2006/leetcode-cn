1d7b5ea6021032dd650d969072e169bc2379f276
Yan Du <red.snapper@foxmail.com>
2026-04-21 11:04:56 +0800

    refactor: ♻️

--- M/1722-minimize-hamming-distance-after-swap-operations/main.rs
+++ M/1722-minimize-hamming-distance-after-swap-operations/main.rs
@@ -19,9 +19,7 @@ impl Solution {
     }
   }
 
-  pub fn minimum_hamming_distance(
-    source: Vec<i32>, target: Vec<i32>, allowed_swaps: Vec<Vec<i32>>,
-  ) -> i32 {
+  pub fn minimum_hamming_distance(source: Vec<i32>, target: Vec<i32>, allowed_swaps: Vec<Vec<i32>>) -> i32 {
     let mut source = source;
     let mut uf_map: HashMap<i32, i32> = HashMap::new();
 
