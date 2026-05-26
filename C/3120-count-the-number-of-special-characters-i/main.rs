2c8241dcf2913da1c9a6b12ecd3621d75d4f6ec5
Yan Du <red.snapper@foxmail.com>
2026-05-26 09:55:46 +0800

    feat: ✨ C/3120-count-the-number-of-special-characters-i/main.rs

--- C/3120-count-the-number-of-special-characters-i/main.rs
+++ C/3120-count-the-number-of-special-characters-i/main.rs
@@ -1,5 +1,25 @@
 impl Solution {
   pub fn number_of_special_chars(word: String) -> i32 {
+    word
+      .as_bytes()
+      .iter()
+      .fold(vec![0; 26], |mut aggr, b| {
+        match b {
+          b'A'..=b'Z' => {
+            aggr[(b - b'A') as usize] |= 2;
+          }
+          _ => {
+            aggr[(b - b'a') as usize] |= 1;
+          }
+        }
+        aggr
+      })
+      .iter()
+      .filter(|&x| *x == 3)
+      .count() as i32
+  }
+
+  pub fn number_of_special_chars2(word: String) -> i32 {
     let mut buf: [(i32, i32); 26] = [(0, 0); 26];
     for c in word.chars() {
       if c.is_uppercase() {
