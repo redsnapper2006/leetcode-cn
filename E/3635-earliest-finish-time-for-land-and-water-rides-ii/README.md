4df50d55abd132ecd84c60f6ccb850eccc94f820
Yan Du <red.snapper@foxmail.com>
2026-06-02 17:39:32 +0800

    feat: ✨ E/3635-earliest-finish-time-for-land-and-water-rides-ii

---
+++ E/3635-earliest-finish-time-for-land-and-water-rides-ii/README.md
@@ -0,0 +1,3 @@
+# No3635
+
+<https://leetcode.cn/problems/earliest-finish-time-for-land-and-water-rides-ii/>
--- E/3635-earliest-finish-time-for-land-and-water-rides-ii/main.rs
+++ E/3635-earliest-finish-time-for-land-and-water-rides-ii/main.rs
@@ -64,37 +64,14 @@ impl Solution {
 
     ans
   }
-
-  pub fn earliest_finish_time2(
-    land_start_time: Vec<i32>, land_duration: Vec<i32>, water_start_time: Vec<i32>, water_duration: Vec<i32>,
-  ) -> i32 {
-    let mut l_min: i32 = land_start_time[0] + land_duration[0];
-    for i in 1..land_start_time.len() {
-      l_min = l_min.min(land_start_time[i] + land_duration[i]);
-    }
-    let mut w_min: i32 = water_start_time[0] + water_duration[0];
-    for i in 1..water_start_time.len() {
-      w_min = w_min.min(water_start_time[i] + water_duration[i]);
-    }
-
-    let mut ans: i32 = 3001;
-    for i in 0..water_start_time.len() {
-      ans = ans.min(l_min.max(water_start_time[i]) + water_duration[i]);
-    }
-    for i in 0..land_start_time.len() {
-      ans = ans.min(w_min.max(land_start_time[i]) + land_duration[i]);
-    }
-
-    ans
-  }
 }
 
 struct Solution {}
 
 fn main() {
-  // println!("{}", Solution::earliest_finish_time(vec![2, 8], vec![4, 1], vec![6], vec![3]));
-  // println!("{}", Solution::earliest_finish_time(vec![99], vec![59], vec![99, 54], vec![85, 20]));
-  // println!("{}", Solution::earliest_finish_time(vec![76, 67], vec![9, 72], vec![86, 90], vec![10, 12]));
-  // println!("{}", Solution::earliest_finish_time(vec![5], vec![3], vec![1], vec![10]));
+  println!("{}", Solution::earliest_finish_time(vec![2, 8], vec![4, 1], vec![6], vec![3]));
+  println!("{}", Solution::earliest_finish_time(vec![99], vec![59], vec![99, 54], vec![85, 20]));
+  println!("{}", Solution::earliest_finish_time(vec![76, 67], vec![9, 72], vec![86, 90], vec![10, 12]));
+  println!("{}", Solution::earliest_finish_time(vec![5], vec![3], vec![1], vec![10]));
   println!("{}", Solution::earliest_finish_time(vec![100000], vec![100000], vec![100000], vec![100000]));
 }
