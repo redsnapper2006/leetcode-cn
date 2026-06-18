fcc980c5b265f6e800b1745c0471150abb039fd8
Yan Du <red.snapper@foxmail.com>
2026-06-18 10:05:24 +0800

    feat: ✨ A/1344-angle-between-hands-of-a-clock/main.rs

--- A/1344-angle-between-hands-of-a-clock/README.md
+++ A/1344-angle-between-hands-of-a-clock/README.md
@@ -1,3 +1,3 @@
 # No1344
 
-https://leetcode-cn.com/problems/angle-between-hands-of-a-clock/
+<https://leetcode-cn.com/problems/angle-between-hands-of-a-clock/>
--- A/1344-angle-between-hands-of-a-clock/main.rs
+++ A/1344-angle-between-hands-of-a-clock/main.rs
@@ -1,9 +1,13 @@
-struct Solution {}
-
 impl Solution {
   pub fn angle_clock(hour: i32, minutes: i32) -> f64 {
-    let mut r: f64 =
-      30 as f64 * (hour as f64) + minutes as f64 / 2 as f64 - 6 as f64 * (minutes as f64);
+    let h = hour as f64;
+    let m = minutes as f64;
+    let r: f64 = (30.0 * h - (6.0 - 0.5) * m).abs();
+    (360.0 - r).min(r)
+  }
+
+  pub fn angle_clock2(hour: i32, minutes: i32) -> f64 {
+    let mut r: f64 = 30 as f64 * (hour as f64) + minutes as f64 / 2 as f64 - 6 as f64 * (minutes as f64);
     if r < 0.0 {
       r = -r;
     }
@@ -14,6 +18,8 @@ impl Solution {
     rr
   }
 }
+
+struct Solution {}
 
 fn main() {
   println!("{}", Solution::angle_clock(10, 20));
