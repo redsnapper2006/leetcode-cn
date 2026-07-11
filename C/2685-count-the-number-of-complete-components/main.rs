8ab83cfc7d687724bca3e1a62cf3ed5ca2506eb3
Yan Du <red.snapper@foxmail.com>
2026-07-11 16:58:41 +0800

    feat: ✨ C/2685-count-the-number-of-complete-components/main.rs

--- C/2685-count-the-number-of-complete-components/main.rs
+++ C/2685-count-the-number-of-complete-components/main.rs
@@ -1,6 +1,52 @@
 use std::collections::HashMap;
 impl Solution {
   pub fn count_complete_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+    let n = n as usize;
+    let mut colors_set: Vec<usize> = (0..n).collect();
+
+    fn find(x: usize, set: &mut Vec<usize>) -> usize {
+      if set[x] != x {
+        set[x] = find(set[x], set);
+      }
+      set[x]
+    }
+
+    fn union(x: usize, y: usize, set: &mut Vec<usize>) {
+      let xx = find(x, set);
+      let yy = find(y, set);
+      if xx != yy {
+        if xx < yy {
+          set[xx] = yy;
+        } else {
+          set[yy] = xx;
+        }
+      }
+    }
+
+    edges.iter().for_each(|e| {
+      union(e[0] as usize, e[1] as usize, &mut colors_set);
+    });
+
+    let mut node_cnt: Vec<i32> = vec![0; n];
+    let mut edge_cnt: Vec<i32> = vec![0; n];
+    (0..n).for_each(|i| {
+      node_cnt[find(i, &mut colors_set)] += 1;
+    });
+
+    edges.iter().for_each(|e| {
+      edge_cnt[find(e[0] as usize, &mut colors_set)] += 1;
+    });
+
+    (0..n).fold(0, |ans, i| {
+      if colors_set[i] == i && node_cnt[i] * (node_cnt[i] - 1) == edge_cnt[i] * 2 {
+        ans + 1
+      } else {
+        ans
+      }
+    })
+  }
+
+  pub fn count_complete_components2(n: i32, edges: Vec<Vec<i32>>) -> i32 {
     let mut cnt: Vec<i32> = vec![0; n as usize];
     let mut colors: Vec<i32> = vec![-1; n as usize];
     let mut m: Vec<Vec<i32>> = vec![vec![]; n as usize];
@@ -36,10 +82,7 @@ impl Solution {
 
     let mut c_cnt: HashMap<i32, i32> = HashMap::new();
     for ii in 0..n {
-      c_cnt
-        .entry(colors[ii as usize])
-        .and_modify(|x| *x += 1)
-        .or_insert(1);
+      c_cnt.entry(colors[ii as usize]).and_modify(|x| *x += 1).or_insert(1);
     }
 
     // println!("{:?}", c_cnt);
