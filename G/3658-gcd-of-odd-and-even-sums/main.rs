7a5f6c9636f969ee9d72a9fb04f26f7eb2890d1f
Yan Du <red.snapper@foxmail.com>
2026-07-15 11:21:57 +0800

    feat: ✨ G/3658-gcd-of-odd-and-even-sums/main.rs

--- G/3658-gcd-of-odd-and-even-sums/main.rs
+++ G/3658-gcd-of-odd-and-even-sums/main.rs
@@ -4,32 +4,18 @@ impl Solution {
   }
 
   pub fn gcd_of_odd_even_sums2(n: i32) -> i32 {
-    let mut odd: i32 = 0;
-    let mut even: i32 = 0;
-    for i in 1..=n * 2 {
-      if i % 2 == 0 {
-        even += i;
-      } else {
-        odd += i;
-      }
-    }
+    let n = n as i64;
+    let mut odd: i64 = n*(n+1);
+    let mut even: i64 = n*n;
 
-    fn gcd(x: i32, y: i32) -> i32 {
-      let mut n1 = x;
-      let mut n2 = y;
-      if (x < y) {
-        n1 = y;
-        n2 = x;
+    let gcd = |mut x :i64, mut y : i64| -> i32 {
+      while y != 0 {
+        let t = y;
+        y = x % y;
+        x = t;
       }
-
-      let mut rem = n1 % n2;
-      while (rem != 0) {
-        n1 = n2;
-        n2 = rem;
-        rem = n1 % n2;
-      }
-      n2
-    }
+      x as i32
+    };
 
     gcd(odd, even)
   }
