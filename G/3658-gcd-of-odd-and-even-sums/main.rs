9d7e69cb021c9844cdd2e856b422ad0ca5b159a6
Yan Du <red.snapper@foxmail.com>
2026-07-15 15:07:12 +0800

    refactor: ♻️

--- G/3658-gcd-of-odd-and-even-sums/main.rs
+++ G/3658-gcd-of-odd-and-even-sums/main.rs
@@ -5,10 +5,10 @@ impl Solution {
 
   pub fn gcd_of_odd_even_sums2(n: i32) -> i32 {
     let n = n as i64;
-    let mut odd: i64 = n*(n+1);
-    let mut even: i64 = n*n;
+    let mut odd: i64 = n * (n + 1);
+    let mut even: i64 = n * n;
 
-    let gcd = |mut x :i64, mut y : i64| -> i32 {
+    let gcd = |mut x: i64, mut y: i64| -> i32 {
       while y != 0 {
         let t = y;
         y = x % y;
