65bcb1af7f6e2a4abcc26d07a5764f047b105871
Yan Du <red.snapper@foxmail.com>
2026-07-25 16:11:49 +0800

    feat: ✨ M/3536-maximum-product-of-two-digits/main.rs

--- M/3536-maximum-product-of-two-digits/main.rs
+++ M/3536-maximum-product-of-two-digits/main.rs
@@ -1,28 +1,18 @@
 impl Solution {
   pub fn max_product(n: i32) -> i32 {
-    let mut dp : Vec<i32> = vec![0;10];
-    let mut n = n as usize;
+    let mut first: i32 = -1;
+    let mut second: i32 = -1;
+    let mut n = n;
     while n > 0 {
-      dp[n %10 ] +=1;
-      n /=10;
-    }
-    let mut m1 : i32 = -1;
-    let mut m2 : i32 = -1;
-
-    let mut idx : usize = 9;
-    while idx > 0 &&( m1 == -1 || m2 == -1 ) {
-      if dp[idx] == 0 {
-        idx -=1;
-        continue;
+      let m = n % 10;
+      if m > first {
+        second = first;
+        first = m;
+      } else if m > second {
+        second = m;
       }
-
-      if m1 == -1 {
-        m1 = idx as i32;
-        dp[idx] -= 1;
-      } else {
-        return m1 * (idx as i32);
-      }
+      n /= 10;
     }
-    0
+    first * second
   }
 }
