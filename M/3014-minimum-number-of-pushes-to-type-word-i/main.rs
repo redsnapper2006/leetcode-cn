44cb9ba732c61a2af9cef1dd0d4fd2f4f7b8461e
Yan Du <red.snapper@foxmail.com>
2026-07-30 10:31:04 +0800

    feat: ✨ M/3014-minimum-number-of-pushes-to-type-word-i/main.rs

--- M/3014-minimum-number-of-pushes-to-type-word-i/main.rs
+++ M/3014-minimum-number-of-pushes-to-type-word-i/main.rs
@@ -1,18 +1,18 @@
-struct Solution {}
-
 impl Solution {
   pub fn minimum_pushes(word: String) -> i32 {
-    let mut buf: Vec<i32> = vec![0; 26];
-    word.as_bytes().iter().for_each(|&b| {
-      buf[(b - b'a') as usize] += 1;
-    });
-    buf.sort();
-    let mut cnt: i32 = 0;
-    buf
-      .iter()
-      .rev()
-      .enumerate()
-      .for_each(|(idx, &v)| cnt += v * (idx as i32 / 8 + 1));
-    cnt
+    let n = word.len() as i32;
+    let m = (n + 7) / 8;
+    m * (m + 1) * 4 - (m * 8 - n) * m
+  }
+
+  pub fn minimum_pushes2(word: String) -> i32 {
+    let mut cnt = [0i32; 26];
+    for &b in word.as_bytes() {
+      cnt[(b - b'a') as usize] += 1;
+    }
+    cnt.sort_unstable_by(|a, b| b.cmp(a));
+    cnt.into_iter().enumerate().fold(0, |ans, (idx, v)| ans + v * (idx as i32 / 8 + 1))
   }
 }
+
+struct Solution {}
