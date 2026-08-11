015ab139cf44e6cfb57d4e671619916944dc6357
Yan Du <red.snapper@foxmail.com>
2026-08-11 10:14:32 +0800

    feat: ✨ S/2996-smallest-missing-integer-greater-than-sequential-prefix-sum/main.rs

--- S/2996-smallest-missing-integer-greater-than-sequential-prefix-sum/main.rs
+++ S/2996-smallest-missing-integer-greater-than-sequential-prefix-sum/main.rs
@@ -1,28 +1,44 @@
 use std::collections::HashSet;
-use std::iter::FromIterator;
 impl Solution {
   pub fn missing_integer(nums: Vec<i32>) -> i32 {
-    let  set : HashSet<i32> =  HashSet::from_iter(nums.iter().cloned());
+    let set: HashSet<i32> = HashSet::from_iter(nums.iter().copied());
 
-    let mut sum : i32 = nums[0];
-    let mut idx : usize = 1;
+    let mut sum: i32 = nums[0];
+    for i in (1..nums.len()) {
+      if nums[i] != nums[i - 1] + 1 {
+        break;
+      }
+      sum += nums[i];
+    }
+
+    while set.contains(&sum) {
+      sum += 1;
+    }
+    sum
+  }
+
+  pub fn missing_integer2(nums: Vec<i32>) -> i32 {
+    let set: HashSet<i32> = HashSet::from_iter(nums.iter().copied());
+
+    let mut sum: i32 = nums[0];
+    let mut idx: usize = 1;
     while idx < nums.len() {
-      if nums[idx]!=nums[idx-1]+1 {
+      if nums[idx] != nums[idx - 1] + 1 {
         break;
       }
 
-        sum += nums[idx];
+      sum += nums[idx];
 
-      idx+=1;
+      idx += 1;
     }
     while set.contains(&sum) {
-      sum+=1;
+      sum += 1;
     }
     sum
   }
 }
 
-struct Solution{}
-fn main(){
-  println!("{}", Solution::missing_integer(vec![1,2,3,14,13,12]));
+struct Solution {}
+fn main() {
+  println!("{}", Solution::missing_integer(vec![1, 2, 3, 14, 13, 12]));
 }
