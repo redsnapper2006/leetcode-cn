e994ec813e9f4e5c98ad68b0cb86a863c78a17bf
Yan Du <red.snapper@foxmail.com>
2026-08-14 10:09:41 +0800

    feat: ✨ M/3090-maximum-length-substring-with-two-occurrences/main.rs

--- M/3090-maximum-length-substring-with-two-occurrences/main.rs
+++ M/3090-maximum-length-substring-with-two-occurrences/main.rs
@@ -1,24 +1,24 @@
 impl Solution {
   pub fn maximum_length_substring(s: String) -> i32 {
-    let mut buf: Vec<i32> = vec![0; 26];
+    let mut cnt: [i32; 26] = [0i32; 26];
 
-    let mut max: usize = 0;
+    let mut max_len: usize = 0;
     let mut start: usize = 0;
     let bb = s.as_bytes();
-    (0..bb.len()).for_each(|bidx| {
+    for bidx in 0..bb.len() {
       let idx = (bb[bidx] - b'a') as usize;
-      buf[idx] += 1;
-      if buf[idx] > 2 {
+      cnt[idx] += 1;
+      if cnt[idx] > 2 {
         while bb[start] != bb[bidx] {
-          buf[(bb[start] - b'a') as usize] -= 1;
+          cnt[(bb[start] - b'a') as usize] -= 1;
           start += 1;
         }
-        buf[(bb[start] - b'a') as usize] -= 1;
+        cnt[idx] -= 1;
         start += 1;
       }
-      max = max.max(bidx - start + 1);
-    });
+      max_len = max_len.max(bidx - start + 1);
+    }
 
-    max as _
+    max_len as _
   }
 }
